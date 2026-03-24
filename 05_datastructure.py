9232a22d3f497dbce1de01fb53cba7d72138298f
hedysurahmat <119188906+hedysurahmat@users.noreply.github.com>
2026-03-24 14:02:53 +0700

    Add data structure examples in 05_datastructure.py

---
+++ 05_datastructure.py
@@ -0,0 +1,13 @@
+# list
+buah = ["apel", "jeruk"]
+
+# tuple
+koordinat = (10, 20)
+
+# dictionary
+data = {"nama": "Hedy", "umur": 20}
+
+# set
+angka = {1, 2, 3}
+
+print(buah, koordinat, data, angka)
